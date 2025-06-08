4df4ca8092b482063dc4b8e8f718b40de4044f58
Codolog <codolog.info@gmail.com>
2025-06-08 18:23:37 +0530

    Added a Register User API

---
+++ src/main/java/com/example/scms/register/RegisterData.java
@@ -0,0 +1,41 @@
+package com.example.scms.register;
+
+public class RegisterData {
+
+    private int id;
+    private String name;
+    private String Email;
+    private int mobile;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(int mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
