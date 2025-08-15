8f9b33a56f62d022e0c098e449df7cf53b95d2b8
shivamdubey00 <techds833@gmail.com>
2025-08-16 03:02:23 +0530

    new code

---
+++ src/main/java/com/example/scms/login/LoginController.java
@@ -0,0 +1,12 @@
+package com.example.scms.login;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+
+
+
+
+}
---
+++ src/main/java/com/example/scms/register/RegisterController.java
@@ -0,0 +1,31 @@
+package com.example.scms.register;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegisterController {
+
+    @PostMapping("/register")
+    public void createAccount()
+    {
+// 1. Check User Exists or not - API for checking Existing users
+// 2. Insert Data via API
+   // 2.1. Response - ture , false
+// 3. API Integrate
+
+
+
+
+        //usage :
+        // 1. Hibernate
+        // 2. Postgres
+        // 3. java 21k
+
+
+
+    }
+
+
+}
